00150e869694cb8ed2582f75a969eb9ea08f398f
Shantanu Mane <shantanu.mane.200@outlook.com>
2022-06-21 17:02:52 +0000

    feat: :zap: week 2 done : master

--- Meta/rough.py
+++ Meta/rough.py
@@ -1,3 +1,11 @@
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax),
+    ]
+)
+
 model.compile(
     optimizer=tf.optimizers.Adam(),
     loss="sparse_categorical_crossentropy",
