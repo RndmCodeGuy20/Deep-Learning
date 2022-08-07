63912fdca349abfa00d64ce266def18399142bb4
maneshantanu20 <manesg@rknec.edu>
2022-08-07 17:00:27 +0530

    meh

--- Meta/rough.py
+++ Meta/rough.py
@@ -1,16 +1,6 @@
-def seasonalPattern(seasonTime):
-    dataPattern = np.where(
-        seasonTime < 0.4,
-        np.cos(seasonTime * 2 * np.pi),
-        1 / np.exp(3 * seasonTime),
-    )
-
-    return dataPattern
-
-
-def seasonality(time, period, amplitude=1, phase=0):
-    seasonTime = ((time + phase) % period) / period
-
-    dataPattern = amplitude * seasonalPattern(seasonTime=seasonTime)
-
-    return dataPattern
+history = model.fit(
+    trainGenerator,
+    steps_per_epoch=8,
+    epochs=15,
+    verbose=1,
+)
